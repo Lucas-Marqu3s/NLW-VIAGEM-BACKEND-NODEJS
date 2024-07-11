65a5a18dd05c6f9202205c6407fdac0517ca244f
Lucas Marques <lucasses10@gmail.com>
2024-07-11 14:53:09 -0300

    feat: Configuring Eslint and Prisma

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,5 +1,29 @@
-import { FastifyInstance } from "fastify";
+/* eslint-disable camelcase */
 
-export function createTrip(app: FastifyInstance){
-    
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/trips',
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(4),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+        }),
+      },
+    },
+    async (request) => {
+      const { destination, starts_at, ends_at } = request.body
+
+      return {
+        destination,
+        starts_at,
+        ends_at,
+      }
+    },
+  )
 }
